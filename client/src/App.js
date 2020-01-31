ca734309028cdb06b4f79fad992ab030ea72776e
Lucas-Moreno <moreno.lucas.82300@hotmail.fr>
2020-01-31 20:19:56 +0100

    connect front/back with api

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  state = {
+    apiResponse: []
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:9000/api`)
+      .then(res => {
+        const apiResponse = res.data;
+        this.setState({ apiResponse });
+      })
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.state.apiResponse}
+      </ul>
+    )
+  }
 }
-
-export default App;
